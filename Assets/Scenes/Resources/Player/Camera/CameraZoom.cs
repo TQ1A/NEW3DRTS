89298eda10e0dae5801720df4c68d6c3852b5634
TQ1A <jumbodragon737@gmail.com>
2023-04-13 02:34:58 +0500

    123

--- Assets/Scenes/Resources/Player/Camera/CameraZoom.cs
+++ Assets/Scenes/Resources/Player/Camera/CameraZoom.cs
@@ -24,25 +24,26 @@ public class CameraZoom : MonoBehaviour
     void Zoom()
     {
         _modify = Mathf.Sqrt(Math.Abs(_rb.position.y)) * 2f;
-        if(_rb.position.y < 0)
+        if(_rb.position.y < _minHigh)
         {
             _modify = 0;
         }
         _zoom = Input.mouseScrollDelta.y;
         Vector3 dir = _myCamera.ScreenPointToRay(Input.mousePosition).direction * _modify;
+        float vec = _zoom * _zoomSpeed * _modify;
 
         if (_rb.position.y < _minHigh)
         {
             _rb.transform.position = Vector3.Lerp(_rb.position, new Vector3(_rb.position.x, _minHigh, _rb.position.z), 10 * Time.deltaTime);
         }
 
-        if (_zoom < 0 && _rb.position.y < _maxHigh)
+        if (_zoom < 0 && _rb.position.y <= _maxHigh)
         {
-            _rb.AddForce(new Vector3(0, dir.y * _zoom * _zoomSpeed * _modify, _zoom * _zoomSpeed * _modify));
+            _rb.AddForce(new Vector3(0, dir.y * vec, vec));
         }
         else if (_zoom > 0 && _rb.position.y >= _minHigh)
         {
-            _rb.AddForce(dir * _zoom * _zoomSpeed * _modify);
+            _rb.AddForce(dir * vec);
         }
     }
 }
